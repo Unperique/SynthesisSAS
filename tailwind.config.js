84a1988fe57d509b72e30f06615a1c5e36d3cb87
andres <andresfelipemoralesmejia@gmail.com>
2025-04-21 11:50:41 -0500

    Mejeras en el encabezado de la pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     extend: {
       colors: {
         'lime': {
-          400: '#a3e635', // Color del logo para referencias
-          500: '#84cc16', // Color principal verde lima
-          600: '#65a30d', // Variante más oscura para hover
+          400: '#a3e635', 
+          500: '#84cc16', 
+          600: '#65a30d', 
         },
       },
     },
